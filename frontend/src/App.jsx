606867d07fa300d2582986b4d6c84e64ff15e0df
felixmathew03 <felixmathewtt@gmail.com>
2025-06-27 08:49:36 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-red-800 h-16 w-full flex items-center justify-center'>
+      <h1 className='text-red-200'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
